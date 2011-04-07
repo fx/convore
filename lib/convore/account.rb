84632b1b82af9c23d0e686b46125fe55ffcdf4eb
RyanonRails <ryan.michael.jones@gmail.com>
2011-04-06 21:55:09 -0600

    added a few account unit tests, mark all read seems to be broken (405?)

--- lib/convore/account.rb
+++ lib/convore/account.rb
@@ -9,14 +9,27 @@ module Convore
 			@password = password
 		end
 		
-		#Use this method to check if the user is properly logged in.
+		#Use this method to check if the user is properly logged in
 		#Check to see if you can even log in..
 		def verified?
 			RestClient.get "https://#{username}:#{password}@convore.com/api/account/verify.json", {:accept => :json}
 		end
 		
+		#Mark all messages as read
+		#getting back a 405, does this even work?
+		def mark_all_read
+			RestClient.get "https://#{username}:#{password}@convore.com/api/account/mark_read.json", {:accept => :json}
+		end
 		
+		#Get members online now
+		def get_members_online
+			RestClient.get "https://#{username}:#{password}@convore.com/api/account/online.json", {:accept => :json}
+		end
 		
-		
+		#Get the users mentions
+		def get_mentions
+			RestClient.get "https://#{username}:#{password}@convore.com/api/account/online.json", {:accept => :json}
+		end
+
 	end
 end
--- test/test_account.rb
+++ test/test_account.rb
@@ -8,11 +8,28 @@ class Test_Account < Test::Unit::TestCase
     @password = 'bot123'
   end
   
-  def test_verified
-    a = Convore::Account.new(@username, @password)
-    response = a.verified?
+  #check if you can login
+  def test_verified?
+    account = Convore::Account.new(@username, @password)
+    response = account.verified?
     assert_equal(response.code, 200)
   end
+  
+  #mark all of the outstanding messages as read
+  #error code 405?
+  #def test_mark_all_read
+  #  account = Convore::Account.new(@username, @password)
+  #  response = account.mark_all_read
+  #  assert_equal(response.code, 200)
+  #end
+  
+  #get all members
+  def test_get_members_online
+    account = Convore::Account.new(@username, @password)
+    response = account.get_members_online
+    assert_equal(response.code, 200)
+  end
+  
 
 end
 
