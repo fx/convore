be35300e699d995822979ed14edbcf530d7019c9
RyanonRails <ryan.michael.jones@gmail.com>
2011-04-10 20:28:42 -0600

    topic calls done in a rush, plane taking off soon. missing a few topic tests

--- lib/convore/topic.rb
+++ lib/convore/topic.rb
@@ -1,5 +1,61 @@
 module Convore
 	class Topic < Base
+		
+		def initialize(username, password)
+			@username = username
+			@password = password
+		end
+		
+		#Get detailed information about the topic
+		def get_topic(topic_id)
+			if topic_id.integer?
+				RestClient.get "https://convore.com/api/topics/#{topic_id}.json"
+			end
+		end
+		
+		#Get the latest messages in a topic. Use the optional until_id parameter
+		#to paginate and get prior messages. Set the optional mark_read parameter
+		#to false to leave the messages as unread
+		def get_topic_messages(topic_id, *hash)
+			if topic_id.integer?
+				RestClient.get "https://convore.com/api/topics/#{topic_id}/messages.json.json",
+				#Optional Parms
+				#until_id
+				#mark_read
+				{hash[0],
+				 hash[1]}
+			end
+		end
+		
+		#Post a new message
+		def create_message(topic_id, message)
+			if topic_id.integer?
+				RestClient.get "https://convore.com/api/topics/#{topic_id}/messages/create.json",
+				{:message => message}
+			end
+		end
+		
+		#Delete a topic. You must be the creator of the topic or a
+		# group admin in order to delete the topic.
+		def delete_topic(topic_id)
+			if topic_id.integer?
+				RestClient.get "https://convore.com/api/topics/#{topic_id}/delete.json"
+			end
+		end
+		
+		#Edit a topic. You must be the creator of the topic or a group admin in order to edit the topic.
+		def edit_topic(topic_id, name)
+			if topic_id.integer?
+				RestClient.get "https://convore.com/api/topics/#{topic_id}/edit.json",
+				{:name => name}
+			end
+		end
+		
+		
+		
+		
+		
+		
 		def self.api
 			'topic'
 		end
