0c50840e27bacd0c0fce1f8d1572040d84332807
RyanonRails <ryan.michael.jones@gmail.com>
2011-04-11 21:39:30 -0600

    user and user tests

--- lib/convore/user.rb
+++ lib/convore/user.rb
@@ -1,4 +1,16 @@
 module Convore
 	class User < Base
+		
+		def initialize(username, password)
+			@username = username
+			@password = password
+		end
+		
+		#Get detailed information about the user
+		def get_info(user_id)
+			if user_id.integer?
+				RestClient.get "https://#{username}:#{password}@convore.com/api/users/#{user_id}.json"
+			end
+		end
 	end
 end
