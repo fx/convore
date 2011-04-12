a93a0ff68acb6b31654c2692d325409dd615823f
RyanonRails <ryan.michael.jones@gmail.com>
2011-04-11 21:32:54 -0600

    message tests

--- lib/convore/message.rb
+++ lib/convore/message.rb
@@ -10,27 +10,16 @@ module Convore
 		# this endpoint will then unstar the message.
 		def star_message(message_id)
 			if message_id.integer?
-				RestClient.post "https://#{username}:#{password}@convore.com/api/messages/#{message_id}/star.json"
+				RestClient.post "https://#{username}:#{password}@convore.com/api/messages/#{message_id}/star.json", {}
 			end
 		end
 		
 		#Delete a message. You must be the creator of the message or a group admin in order to delete the message.
-		def get_message(message_id)
+		def delete_message(message_id)
 			if message_id.integer?
-				RestClient.post "https://#{username}:#{password}@convore.com/api/messages/#{message_id}/delete.json"
+				RestClient.post "https://#{username}:#{password}@convore.com/api/messages/#{message_id}/delete.json", {}
 			end
 		end
-		
-		#Delete a message. You must be the creator of the message or a group admin in order to delete the message.
-		def get_message(message_id)
-			if message_id.integer?
-				RestClient.post "https://#{username}:#{password}@convore.com/api/messages/#{message_id}/star.json"
-			end
-		end
-		
-		
-		def text
-			message
-		end
+
 	end
 end
